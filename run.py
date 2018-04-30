cbc3a0e6ef0f8008791a962cf150d414f2507208
Timothy <hendr522@umn.edu>
2018-04-30 16:04:00 -0500

    redesigned run.py to fit in with a different control script

--- run.py
+++ run.py
@@ -1,51 +1,48 @@
+#!/home/lnpi14-raid1/timothy-data-lnpi14/nipype_python_2.7.10/bin python
+
 from glob import glob
 import os
-import sys
-from pydicom import read_file
 import shutil
-from joblib import Parallel, delayed
+import argparse
 
 
 #book keeping and global variables
 
-# does udocker exist within path?
-if not 'linux' in sys.platform:
-	raise EnvironmentError('platform is not linux, must exit.')
-if os.system('which udocker') != 0:
-	raise EnvironmentError('udocker does not exist in path, either download udocker or place within PATH environment variable.')
-
-subjdir = "/home/lnpi15-raid2/kathryn-data-lnpi15/275_BRIDGES"
-subj_glob = "[4-5]????"
-raw_datadir = "/home/range2-raid1/timothy/TEMP"
-bidsdir = "/home/lnpi15-raid2/kathryn-data-lnpi15/275_BRIDGES/BIDS"
-heuristics_script= "275_BRIDGES_heuristics.py"
-docker_location = "/home/lnpi14-raid1/timothy-data-lnpi14/nipype_python_2.7.10/bin/udocker"
+parser = argparse.ArgumentParser(description='Script that controls BIDS conversion for individual studies')
+parser.add_argument('--top_level_dir', help="The directory this script is being called from, it should also have the heuristics script within.")
+parser.add_argument('--output_dir', help="The directory that the BIDS data will be outputted to")
+parser.add_argument('--temp_dir', help='The directory that will temporarily house dicom directories.')
+parser.add_argument('--study_name', help="What is the shorthand name for this study?")
+parser.add_argument('--proc_id', help="5 digit proc number (AKA event ID from GRID)")
+parser.add_argument('--subj_id', help="4 digit subject number (AKA subject id from GRID)")
+parser.add_argument('--container', help="location of docker or udocker install")
 
 
-os.chdir(bidsdir)
+args = parser.parse_args()
+
+top_level_dir = args.top_level_dir
+output_dir = args.output_dir
+temp_dir = args.temp_dir
+study_name = args.study_name
+proc_id = args.proc_id
+subj_id = args.subj_id
+container = args.container
+heuristics_script = study_name + "_heuristics.py"
+
+if not os.path.exists(os.path.join(top_level_dir, output_dir)):
+	os.makedirs(os.path.join(top_level_dir,output_dir))
+
+if not os.path.exists(os.path.join(temp_dir, subj_id)):
+	os.makedirs(os.path.join(temp_dir, subj_id))
+
+if not os.path.exists(heuristics_script):
+	raise EnvironmentError("Heuristics script for " + study_name + " does not exist, it must be in the top level directory: " + top_level_dir)
 
 
-def batch(accession_number):
-	accession_number = os.path.basename(accession_number)
-	if not os.path.isdir(os.path.join(bidsdir, 'BIDS_output', 'sub-'+accession_number)) \
-			or glob(bidsdir +'/BIDS_output/'+ '*' + '/ses-'+accession_number) == []:
-		if not glob("%s/*%s*" % (raw_datadir, accession_number)) and not glob("%s/*/*%s*" % (raw_datadir, accession_number)):
-			os.system("%s/gunzip_proc.sh %s" % (raw_datadir, accession_number))
-		raw_file = read_file(glob("%s/PN-%s*/*/*0001.dcm" % (raw_datadir, accession_number))[0])
-		subj = str(raw_file.PatientName).split("^")[0][-4:]
-		if not os.path.exists(os.path.join(raw_datadir, subj)):
-			os.makedirs(os.path.join(raw_datadir, subj))
-		os.rename(glob("%s/PN-%s*" % (raw_datadir, accession_number))[0], "%s/%s" % (raw_datadir, accession_number))
-		shutil.move(os.path.join(raw_datadir, accession_number), (os.path.join(raw_datadir, subj)))
-		os.system('/bin/bash -c "%s run -v $PWD:/home/tim -v %s/:/home/tim/data \
-		-v %s/%s:/home/tim/%s run_heudiconv -d /home/tim/data/{subject}/{session}/*/*.dcm -s %s \
-		-ss %s --overwrite -o /home/tim/BIDS_output -c dcm2niix -f /home/tim/%s -b"'
-		          % (docker_location, raw_datadir, bidsdir, heuristics_script, heuristics_script, subj, accession_number,
-		             heuristics_script))
-		shutil.rmtree("%s/%s" % (raw_datadir, subj))
-	else:
-		print 'this participant already exists, no need to convert'
+os.rename(glob("%s/PN-%s*" % (temp_dir, proc_id))[0], "%s/%s" % (temp_dir, proc_id))
+shutil.move(os.path.join(temp_dir, proc_id), (os.path.join(temp_dir, subj_id)))
+os.system('/bin/bash -c "%s run -v $PWD:/home/tim -v %s/:/home/tim/data \
+-v %s/%s:/home/tim/%s run_heudiconv -d /home/tim/data/{subj_idect}/{session}/*/*.dcm -s %s \
+-ss %s --overwrite -o /home/tim/%s -c dcm2niix -f /home/tim/%s -b"'
+		  % (container, temp_dir, top_level_dir, heuristics_script, heuristics_script, subj_id, proc_id, output_dir, heuristics_script))
 
-#Parallelize processing
-Parallel(n_jobs=6,backend='multiprocessing',verbose=1)(delayed(batch)(accession_number) \
-		 for accession_number in sorted(glob("%s/%s" % (subjdir, subj_glob))))
